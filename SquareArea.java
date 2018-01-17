c5be7301b1cb0bdca79eb8844592a0f38efb2857
AsenGeorgiev <35175468+AsenGeorgiev@users.noreply.github.com>
2018-01-17 11:43:20 +0200

    01. Square Area

---
+++ SquareArea.java
@@ -0,0 +1,11 @@
+package java2;
+import java.util.Scanner;
+public class SquareArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = Double.parseDouble(scanner.nextLine());
+        double area = a * a;
+        System.out.println(area);
+        }
+    }
+
